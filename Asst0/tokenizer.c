d83bc101d3d4d8cdddc24ce0e84f5bf11b4135e1
Chris Naporlee <chrisn731@gmail.com>
2020-09-23 03:42:18 -0400

    Final solo changes

--- Asst0/tokenizer.c
+++ Asst0/tokenizer.c
@@ -27,20 +27,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 struct input_tokens {
 	char **input;
 	int num_of_tokens;
 };
 
+struct C_token {
+	const char *name;
+	const char *operator;
+};
+
 void die(const char *msg);
 void strcopy(const char *src, char *dest, int n);
+void print_token(const char *type, const char *tok);
 int is_letter(char c);
 int is_number(char c);
 int is_float (char *str);
 int is_hex(char *str);
 int num_of_tokens(char *arg);
 struct input_tokens *split_tokens(char *arg);
+
+const struct C_token C_tokens[] = {
+	{"left parenthesis", "("},
+	{"right parenthesis", ")"},
+	{"left brace", "["},
+	{"right brace", "]"},
+	{"structure member", "."},
+	{"structure pointer", "->"},
+	{"sizeof", "sizeof"},
+	{"comma", ","},
+	{"negate", "!"},
+	{"1s complement", "~"},
+	{"shift right", ">>"},
+	{"shift left", "<<"},
+	{"bitwise XOR", "^"},
+	{"bitwise OR", "|"},
+	{"increment", "++"},
+	{"decrement", "--"},
+	{"addition", "+"},
+	{"division", "/"},
+	{"logical OR", "||"},
+	{"logical AND", "&&"},
+	{"conditional true", "?"},
+	{"conditional false", ":"},
+	{"equality test", "=="},
+	{"inequality test", "!="},
+	{"less than test", "<"},
+	{"greater than test", ">"},
+	{"less than or equal test", "<="},
+	{"greater than or equal test", ">="},
+	{"assignment", "="},
+	{"plus equals", "+="},
+	{"minus equals", "-="},
+	{"times equals", "*="},
+	{"divide equals", "/="},
+	{"mod equals", "%="},
+	{"shift right equals", ">>="},
+	{"shift left equals", "<<="},
+	{"bitwise AND equals", "&="},
+	{"bitwise XOR equals", "^="},
+	{"bitwise OR equals", "|="},
+	{"AND/address operator", "&"},
+	{"minus/subtract operator", "-"},
+	{"multiply/dereference operator", "*"},
+};
 
 /*
  * Error exit function.
@@ -105,6 +157,11 @@ int is_hex(char *str)
 		((str[1] == 'x') || str[1] == 'X'));
 }
 
+void print_token(const char *type, const char *token)
+{
+	printf("%s: \"%s\"\n", type, token);
+}
+
 /*
  * Takes in a pointer to a string of tokens and returns num of
  * tokens as int.
@@ -118,10 +175,10 @@ int num_of_tokens(char *arg)
 		if (*parse != ' ') {
 			do {
 				parse++;
-			} while (*parse != ' ' && *parse != '\0');
+			} while (!isspace(*parse) && *parse != '\0');
 			num++;
 		}
-		while (*parse == ' ' && *parse != '\0') {
+		while (isspace(*parse) && *parse != '\0') {
 			parse++;
 		}
 	}
@@ -140,13 +197,10 @@ struct input_tokens *split_tokens(char *arg)
 
 	tokens = malloc(sizeof(*tokens));
 	tokens->num_of_tokens = num_of_tokens(arg);
-	if (tokens->num_of_tokens == 0)
-		die("Input contains no tokens to be parsed");
-
 	tokens->input = malloc(sizeof(*tokens->input) * tokens->num_of_tokens);
 	while (1) {
 		/* Skip all white space */
-		while (*parser == ' ')
+		while (isspace(*parser))
 			parser++;
 
 		if (*parser == '\0')
@@ -155,7 +209,7 @@ struct input_tokens *split_tokens(char *arg)
 		/* Track the beginning of token and it's length */
 		start_of_token = parser;
 		strlen = 0;
-		while (*parser != ' ' && *parser != '\0') {
+		while (!isspace(*parser) && *parser != '\0') {
 			parser++;
 			strlen++;
 		}
@@ -171,7 +225,10 @@ int main(int argc, char **argv)
 {
 	if (argc < 2)
 		die("Please include a string.");
-
+	if (argc > 2)
+		die("Received too many inputs.");
+	if (num_of_tokens(argv[1]) == 0)
+		return 0;
 	/* Parse command line args */
 
 	return 0;
