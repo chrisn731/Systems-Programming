b4f4e4d7d3ca318285e98877dae06fa873fe843d
Chris Naporlee <chrisn731@gmail.com>
2020-10-12 22:54:50 -0400

    show time calculation

--- Asst1/memgrind.c
+++ Asst1/memgrind.c
@@ -1,11 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "mymalloc.h"
 
 
 int main(void)
 {
+	/* This is just here to show how we will be computing time taken */
+	clock_t start, end;
+	double total_time;
+
+	/* Start the clock */
+	start = clock();
+
+	/* Beep Boop ... Code goes here */
+
+	/* End the clock */
+	end = clock();
+
+	/* Calculate time taken */
+	total_time = (double)(end - start) / CLOCKS_PER_SEC;
 
 	return 0;
 }
