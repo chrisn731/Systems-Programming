cc954c02ff37755e4398b39c9e85018bf4700d79
Chris Naporlee <chrisn731@gmail.com>
2020-10-17 23:34:35 -0400

    more progress

--- Asst1/mymalloc.c
+++ Asst1/mymalloc.c
@@ -31,12 +31,13 @@ struct header_data {
 	unsigned short block_size: 15;
 	unsigned short free: 1;
 };
+#define BYTES_TO_NEXT_META(size) (sizeof(struct header_data) + (size))
 
 static char myblock[HEAP_SIZE] = {0};
 
 static void die(const char *err, const char *fname, const int line_num)
 {
-	fprintf(stderr, "::[File: %s : Line: %d] Error: %s\n", fname, line_num, err);
+	fprintf(stderr, "::[File: %s: Line: %d] Error: %s\n", fname, line_num, err);
 	exit(1);
 }
 
@@ -52,7 +53,7 @@ void *mymalloc(size_t size, const char *filename, const int line_number)
 {
 	struct header_data *meta, *next_block;
 	char *heap_byte = myblock;
-	char *const heap_boundary = myblock + HEAP_SIZE;
+	const char *const heap_boundary = myblock + HEAP_SIZE;
 
 	if (size == 0)
 		return NULL;
@@ -61,13 +62,12 @@ void *mymalloc(size_t size, const char *filename, const int line_number)
 	if (!myblock[0] && !myblock[1])
 		initialize_heap();
 
-	while (heap_byte < heap_boundary) {
+	do {
 		meta = (struct header_data *) heap_byte;
-		if (meta->free && ((meta->block_size + sizeof(*meta)) >= size))
+		if (meta->free && ((meta->block_size + sizeof(*meta)) > size))
 			break;
 		heap_byte += (sizeof(*meta) + meta->block_size);
-
-	}
+	} while (heap_byte < heap_boundary);
 
 	if (heap_byte >= heap_boundary)
 		FATAL("Heap out of memory.");
@@ -77,24 +77,74 @@ void *mymalloc(size_t size, const char *filename, const int line_number)
 	next_block->block_size = meta->block_size - size;
 	meta->free = 0;
 	meta->block_size = size;
-	return (void *) heap_byte + sizeof(*meta);
+	return (void *) (heap_byte + sizeof(*meta));
 }
 
 
 static int not_in_range(void *ptr)
 {
 	char *ptr_to_free = (char *) ptr;
+	char *heap_boundary = myblock + HEAP_SIZE;
 
-	return ((ptr_to_free < myblock) ||
-		(ptr_to_free >= (myblock + HEAP_SIZE)));
+	return (ptr_to_free < myblock) || (ptr_to_free >= heap_boundary);
+}
+
+static int non_mymalloc_ptr(void *ptr)
+{
+	struct header_data *meta;
+	const char *const heap_boundary = myblock + HEAP_SIZE;
+	char *heap_byte = myblock;
+	char *block_ptr;
+
+	do {
+		block_ptr = heap_byte + sizeof(*meta);
+		if (ptr == block_ptr)
+			return 0;
+		meta = (struct header_data *) heap_byte;
+		heap_byte += sizeof(*meta) + meta->block_size;
+	} while (heap_byte < heap_boundary);
+
+	return 1;
+}
+
+static void coalesce_blocks(void)
+{
+	struct header_data *first_meta, *next_meta;
+	const char *const heap_boundary = myblock + HEAP_SIZE;
+	char *heap_byte = myblock;
+
+	do {
+		first_meta = (struct header_data *) heap_byte;
+		heap_byte += first_meta->block_size + sizeof(*first_meta);
+		next_meta = (struct header_data *) heap_byte;
+		if (first_meta->free) {
+			while (next_meta->free) {
+				first_meta->block_size += next_meta->block_size + sizeof(*next_meta);
+				heap_byte += next_meta->block_size + sizeof(*next_meta);
+				next_meta = (struct header_data *) heap_byte;
+			}
+		}
+	} while (heap_byte < heap_boundary);
 }
 
 void myfree(void *ptr, const char *filename, const int line_number)
 {
+	struct header_data *meta;
 	char *block_ptr = (char *) ptr;
 
 	if (!block_ptr)
 		FATAL("Attempting to free NULL pointer.");
 	if (not_in_range(ptr))
 		FATAL("Attempting to free pointer not in range.");
+	if (non_mymalloc_ptr(ptr))
+		FATAL("Attempting to free nonmalloc'd pointer");
+
+	block_ptr -= sizeof(*meta);
+	meta = (struct header_data *) (block_ptr -= sizeof(*meta));
+	if (!meta->free)
+		FATAL("Attempting to redudantly free pointer");
+	else
+		meta->free = 1;
+
+	coalesce_blocks();
 }
