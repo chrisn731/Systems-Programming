306008bb6cb30535b98f0501a9eb18978ca55bff
Chris Naporlee <chrisn731@gmail.com>
2020-10-18 03:35:02 -0400

    Added lots of comments and fixed small things

--- Asst1/mymalloc.c
+++ Asst1/mymalloc.c
@@ -32,15 +32,26 @@ struct header_data {
 	unsigned short free: 1;
 };
 #define BYTES_TO_NEXT_META(size) (sizeof(struct header_data) + (size))
+#define HEADER_DATA(x) (struct header_data *)(x)
 
 static char myblock[HEAP_SIZE] = {0};
 
+/*
+ * Purpose: Error exit function. Prints the error as well as the file and line number
+ * of where the error occured.
+ * Return Value: Does not return.
+ */
 static void die(const char *err, const char *fname, const int line_num)
 {
 	fprintf(stderr, "::[File: %s: Line: %d] Error: %s\n", fname, line_num, err);
 	exit(1);
 }
 
+/*
+ * Purpose: Initlialize the first 2 bytes of the heap to be meta data. This
+ * allows future blocks to be built and split off from this first block.
+ * Return Value: None.
+ */
 static void initialize_heap(void)
 {
 	struct header_data *meta = (struct header_data *) myblock;
@@ -49,6 +60,10 @@ static void initialize_heap(void)
 	meta->free = 1;
 }
 
+/*
+ * Purpose: Returns a pointer to a chunk of memory in our "heap".
+ * Return Value: Pointer to the first byte of allocated memory.
+ */
 void *mymalloc(size_t size, const char *filename, const int line_number)
 {
 	struct header_data *meta, *next_block;
@@ -62,9 +77,13 @@ void *mymalloc(size_t size, const char *filename, const int line_number)
 	if (!myblock[0] && !myblock[1])
 		initialize_heap();
 
+	/*
+	 * go through the heap to find an empty block that can fit the block size
+	 * and the meta data for the block that will be after.
+	 */
 	do {
 		meta = (struct header_data *) heap_byte;
-		if (meta->free && ((meta->block_size + sizeof(*meta)) > size))
+		if (meta->free && (meta->block_size >= (size + sizeof(*meta))))
 			break;
 		heap_byte += (sizeof(*meta) + meta->block_size);
 	} while (heap_byte < heap_boundary);
@@ -80,7 +99,10 @@ void *mymalloc(size_t size, const char *filename, const int line_number)
 	return (void *) (heap_byte + sizeof(*meta));
 }
 
-
+/*
+ * Purpose: Takes in a pointer and tests to see if it is in range of our heap.
+ * Return Value: 0 if in range, non-zero otherwise.
+ */
 static int not_in_range(void *ptr)
 {
 	char *ptr_to_free = (char *) ptr;
@@ -89,6 +111,11 @@ static int not_in_range(void *ptr)
 	return (ptr_to_free < myblock) || (ptr_to_free >= heap_boundary);
 }
 
+/*
+ * Purpose: Takes in a pointer and tests to see if it is a pointer to an allocated
+ * block of memory.
+ * Return Value: 0 if it is a valid mymalloc'd pointer, non-zero otherwise.
+ */
 static int non_mymalloc_ptr(void *ptr)
 {
 	struct header_data *meta;
@@ -107,6 +134,11 @@ static int non_mymalloc_ptr(void *ptr)
 	return 1;
 }
 
+/*
+ * Purpose: Goes through the heap and combines free blocks into one block. Gets
+ * called after every call to myfree().
+ * Return Value: None.
+ */
 static void coalesce_blocks(void)
 {
 	struct header_data *first_meta, *next_meta;
@@ -127,11 +159,17 @@ static void coalesce_blocks(void)
 	} while (heap_byte < heap_boundary);
 }
 
+/*
+ * Purpose: Frees an allocated section of memory from our heap to be used
+ * later.
+ * Return Value: None.
+ */
 void myfree(void *ptr, const char *filename, const int line_number)
 {
 	struct header_data *meta;
 	char *block_ptr = (char *) ptr;
 
+	/* Check main 3 error cases */
 	if (!block_ptr)
 		FATAL("Attempting to free NULL pointer.");
 	if (not_in_range(ptr))
@@ -141,10 +179,12 @@ void myfree(void *ptr, const char *filename, const int line_number)
 
 	block_ptr -= sizeof(*meta);
 	meta = (struct header_data *) block_ptr;
+	/* check if the block has been freed already */
 	if (meta->free)
 		FATAL("Attempting to redudantly free pointer");
 	else
 		meta->free = 1;
 
+	/* Go through the heap and combine free'd blocks */
 	coalesce_blocks();
 }
